7e172639d476a29db2779ee155b65281043ec47e
William Qian <william.z.qian@gmail.com>
2023-05-21 17:58:20 +0800

    Finished Home page

---
+++ src/app/components/card.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Card(props) {
+    return (
+        <div
+            className="group justify-center rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <Link href={props.href} className="flex flex-row justify-center pt-2 pb-2 text-gray-600">
+                <h2 className="mb-3 text-1xl font-mono">
+                    {props.name}{''}
+                </h2>
+                <div
+                    className="inline-block transition-transform duration-300 group-hover:translate-x-5 motion-reduce:transform-none">
+                    -&gt;
+                </div>
+            </Link>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,91 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            translate: ['responsive', 'group-hover'],
+            backgroundImage: {
+                "gradient-radial":
+                    "radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+            },
+            animation: {
+                "fade-in": "fade-in 3s ease-in-out forwards",
+                title: "title 3s ease-out forwards",
+                "fade-left": "fade-left 3s ease-in-out forwards",
+                "fade-right": "fade-right 3s ease-in-out forwards",
+                'gradient-anim': 'gradient-anim 3s ease infinite',
+            },
+            keyframes: {
+                "fade-in": {
+                    "0%": {
+                        opacity: "0%",
+                    },
+                    "75%": {
+                        opacity: "0%",
+                    },
+                    "100%": {
+                        opacity: "100%",
+                    },
+                },
+                "fade-left": {
+                    "0%": {
+                        transform: "translateX(100%)",
+                        opacity: "0%",
+                    },
+
+                    "30%": {
+                        transform: "translateX(0%)",
+                        opacity: "100%",
+                    },
+                    "100%": {
+                        opacity: "0%",
+                    },
+                },
+                "fade-right": {
+                    "0%": {
+                        transform: "translateX(-100%)",
+                        opacity: "0%",
+                    },
+
+                    "30%": {
+                        transform: "translateX(0%)",
+                        opacity: "100%",
+                    },
+                    "100%": {
+                        opacity: "0%",
+                    },
+                },
+                'gradient-anim': {
+                    '0%': {'background-position': '0% 50%'},
+                    '50%': {'background-position': '100% 50%'},
+                    '100%': {'background-position': '0% 50%'},
+                },
+                title: {
+                    "0%": {
+                        "line-height": "0%",
+                        "letter-spacing": "0.25em",
+                        opacity: "0",
+                    },
+                    "25%": {
+                        "line-height": "0%",
+                        opacity: "0%",
+                    },
+                    "80%": {
+                        opacity: "100%",
+                    },
+
+                    "100%": {
+                        "line-height": "100%",
+                        opacity: "100%",
+                    },
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
+
