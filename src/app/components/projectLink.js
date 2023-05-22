39fe5d1a6c75bbdf99cc42e89fa6024f76b2cead
William Qian <william.z.qian@gmail.com>
2023-05-22 15:12:18 +0800

    Finished Projects

--- src/app/components/projectLink.js
+++ src/app/components/projectLink.js
@@ -1,25 +1,25 @@
 import Link from "next/link";
-import {Eye, View} from "lucide-react";
+import {Eye, Star ,View} from "lucide-react";
 
-export const ProjectLink = () => {
+export const ProjectLink = (props) => {
     return (
-        <Link href="/">
+        <Link href={props.href} target="_blank">
             <article className="p-4 md:p-8">
                 <div className="flex justify-between gap-2 items-center">
 					<span
-                        className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange mb-2">
-						{new Date().toISOString()}
+                        className="font-mono text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange mb-2">
+						{new Date(props.date).toISOString().split('T')[0]}
 					</span>
-                    <span className="text-zinc-500 text-xs  flex items-center gap-1">
-						<Eye className="w-4 h-4"/>{" "}
-                        1000
+                    <span className="font-mono text-zinc-500 text-xs  flex items-center gap-1">
+						<Star className="w-4 h-4"/>{" "}
+                        {props.stars}
 					</span>
                 </div>
-                <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-                    Test Title is here check it
+                <h2 className="z-20 font-mono text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+                    {props.title}
                 </h2>
-                <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                    Test Description Test Description Test Description Test Description Test Description Test Description Test Description
+                <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200 font-mono">
+                    {props.description !== null ? props.description : "This guy was lazy, he didn't write an description."}
                 </p>
             </article>
         </Link>
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,53 +1,64 @@
-import Main from "@/app/components/main";
+'use client';
 import Content from "@/app/components/content";
 import Bar from "@/app/components/bar";
 import {ProjectCard} from "@/app/components/projectCard";
 import {ProjectLink} from "@/app/components/projectLink";
+import useSWR from "swr";
 
 export default function Page() {
-    const sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    const fetcher = url => fetch(url).then(res => res.json())
+    const {data, error} = useSWR('https://api.github.com/users/dukechain2333/repos', fetcher);
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
     return (
         <Content>
             <Bar/>
-            <div className="mt-28 mb-10 2xl:w-1/2 2xl:left-auto 2xl:right-auto xl:w-2/3 xl:left-auto xl:right-auto lg:w-2/3 lg:left-auto lg:right-auto sm:w-full px-5">
+            <div
+                className="mt-28 mb-10 2xl:w-1/2 2xl:left-auto 2xl:right-auto xl:w-2/3 xl:left-auto xl:right-auto lg:w-2/3 lg:left-auto lg:right-auto sm:w-full px-5">
                 <div className="max-w-2xl mx-auto lg:mx-0">
-                    <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+                    <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl font-mono">
                         Projects
                     </h2>
-                    <p className="mt-4 text-zinc-400">
-                        Some of the projects are from work and some are on my own time.
+                    <p className="mt-4 text-zinc-400 font-mono">
+                        Those are my projects from GitHub.
                     </p>
                 </div>
             </div>
 
             <div className="w-full h-px bg-zinc-800"/>
 
-            <div className="flex px-5 mt-10 2xl:w-1/2 2xl:left-auto 2xl:right-auto xl:w-2/3 xl:left-auto xl:right-auto lg:w-2/3 lg:left-auto lg:right-auto sm:w-full">
+            <div
+                className="flex px-5 mt-10 2xl:w-1/2 2xl:left-auto 2xl:right-auto xl:w-2/3 xl:left-auto xl:right-auto lg:w-2/3 lg:left-auto lg:right-auto sm:w-full">
                 <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
                     <div className="grid grid-cols-1 gap-4">
-                        {sorted
+                        {data
                             .filter((_, i) => i % 3 === 0)
                             .map((project, index) => (
-                                <ProjectCard key={index}>
-                                    <ProjectLink/>
+                                <ProjectCard key={project.id}>
+                                    <ProjectLink href={project.html_url} date={project.created_at} title={project.name}
+                                                 stars={project.stargazers_count} description={project.description}/>
                                 </ProjectCard>
                             ))}
                     </div>
                     <div className="grid grid-cols-1 gap-4">
-                        {sorted
+                        {data
                             .filter((_, i) => i % 3 === 1)
                             .map((project, index) => (
-                                <ProjectCard key={index}>
-                                    <ProjectLink/>
+                                <ProjectCard key={project.id}>
+                                    <ProjectLink href={project.html_url} date={project.created_at} title={project.name}
+                                                 stars={project.stargazers_count} description={project.description}/>
                                 </ProjectCard>
                             ))}
                     </div>
                     <div className="grid grid-cols-1 gap-4">
-                        {sorted
+                        {data
                             .filter((_, i) => i % 3 === 2)
                             .map((project, index) => (
-                                <ProjectCard key={index}>
-                                    <ProjectLink/>
+                                <ProjectCard key={project.id}>
+                                    <ProjectLink href={project.html_url} date={project.created_at} title={project.name}
+                                                 stars={project.stargazers_count} description={project.description}/>
                                 </ProjectCard>
                             ))}
                     </div>
